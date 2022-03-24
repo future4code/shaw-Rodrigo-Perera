298253d56f4df7c4cd9794f001450c737f77f23e
Rodrigo Pernambuco <rodrigoperera1@hotmail.com>
2022-03-24 18:39:54 -0300

    exercicio insta4 p2 modulo2

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -10,26 +10,93 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state ={
+    perfis: [
+        {
+          nomeUsuario: "Paulinha",
+          fotoUsuario: "https://picsum.photos/50/50?a",
+          fotoPost: "https://picsum.photos/200/150?d"
+        },
+        {
+          nomeUsuario: "Rodrigo",
+          fotoUsuario: "https://picsum.photos/50/50?b",
+          fotoPost: "https://picsum.photos/200/150?e"
+        },
+        {
+          nomeUsuario: "Arthur",
+          fotoUsuario: "https://picsum.photos/50/50?c",
+          fotoPost: "https://picsum.photos/200/150?f"
+        }
+    ],
+
+      inputNomeUsuario: "",
+      inputFotoUsuario: "",
+      inputFotoPost: "",
+  };
+
+    adicionaPerfil = () => {
+      const adicionaPerfil = {
+        nomeUsuario: this.state.inputNomeUsuario,
+        fotoUsuario: this.state.inputFotoUsuario,
+        fotoPost: this.state.inputFotoPost
+      };
+
+      const novoPerfil = [...this.state.perfis, adicionaPerfil];
+
+      this.setState({perfis: novoPerfil});
+      this.setState({
+        inputNomeUsuario: "",
+        inputFotoUsuario: "",
+        inputFotoPost: ""
+      });
+    };
+
+    onChangeInputPerfil = (event) => {
+      this.setState({ inputNomeUsuario: event.target.value });
+    };
+
+    onChangeInputFotoPerfil = (event) => {
+      this.setState({ inputFotoUsuario: event.target.value });
+    };
+
+    onChangeInputFotoPost = (event) => {
+      this.setState({ inputFotoPost: event.target.value });
+    };
+
   render() {
+    const listaDePerfil = this.state.perfis.map((perfil) => {
+      return (
+        <Post
+        nomeUsuario={perfil.nomeUsuario}
+        fotoUsuario={perfil.fotoUsuario}
+        fotoPost={perfil.fotoPost}
+        />
+      )
+    });
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50?a'}
-          fotoPost={'https://picsum.photos/200/150?d'}
-        />
+        <div>
+          <input 
+            value={this.state.inputNomeUsuario}
+            onChange={this.onChangeInputPerfil}
+            placeholder={"Nome de Usuario"}
+          />
 
-        <Post
-          nomeUsuario={'Rodrigo'}
-          fotoUsuario={'https://picsum.photos/50/50?b'}
-          fotoPost={'https://picsum.photos/200/150?e'}
-        />
+          <input 
+            value={this.state.inputFotoUsuario}
+            onChange={this.onChangeInputFotoPerfil}
+            placeholder={"Foto de Perfil"}
+          />
 
-        <Post
-          nomeUsuario={'Arthur'}
-          fotoUsuario={'https://picsum.photos/50/50?c'}
-          fotoPost={'https://picsum.photos/200/150?f'}
-        />
+          <input 
+            value={this.state.inputFotoPost}
+            onChange={this.onChangeInputFotoPost}
+            placeholder={"Poste Sua Foto"}
+          />
+          <button onClick={this.adicionaPerfil}>Adicionar Perfil</button>
+        </div>
+
+        {listaDePerfil}
       </MainContainer>
     );
   }
