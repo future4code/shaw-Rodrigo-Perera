447723f6745675c026e0395078dc31e3b511a572
Rodrigo Pernambuco <rodrigoperera1@hotmail.com>
2022-04-07 16:15:47 -0300

    projeto labefy modulo 2

---
+++ modulo2/projeto-labefy/src/App.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+import styled from "styled-components"
+import react from "react"
+
+export default class App extends React.Component {
+  
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
