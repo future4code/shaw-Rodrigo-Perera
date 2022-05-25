ecbb97dd5a56ff813a9ea294149b905ad0bdb534
Rodrigo Pernambuco <rodrigoperera1@hotmail.com>
2022-05-25 14:51:39 -0300

    exercicios introducao typescript modulo4

---
+++ modulo4/intro-typescript/src/Exercicio2.ts
@@ -0,0 +1,4 @@
+function imprimeTresCoresFavoritas(cor1:string, cor2:string, cor3:string):string[] {
+    return ([cor1, cor2, cor3])
+ }
+ console.log(imprimeTresCoresFavoritas("vermelho", "azul", "verde"))
