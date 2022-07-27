5d39cd56ffe8f98a5b012cb32f2532c1023f59bf
Rodrigo Pernambuco <rodrigoperera1@hotmail.com>
2022-07-27 18:19:07 -0300

    trabalho finalizado

--- modulo6/case2-brainn/src/components/Loteria.js
+++ modulo6/case2-brainn/src/components/Loteria.js
@@ -12,28 +12,73 @@ const loteriaCores = {
 
 const Titulo = styled.h1`
   text-transform: uppercase;
+  margin: 15% 0 15% 0;
 `;
 
 const Background = styled.div`
   background-color: ${(props) => loteriaCores[props.id]};
-  padding: 25vh 0 25vh 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+
+  h3 {
+    margin: 15% 0 15% 0;
+  }
+
+  select {
+    margin-top: 15%;
+    width: 50%;
+    text-align: center;
+    margin-left: 25%;
+    height: 30px;
+    border: none;
+    border-radius: 5px;
+  }
+`;
+
+const NumberDiv = styled.div`
+  background-color: #efefef;
+  flex: 1;
+  display: table;
+  table-layout: fixed;
+  justify-content: center;
+  margin-left: 3px;
+`;
+
+const Numeros = styled.li`
+  float: left;
+  display: table-cell;
+  overflow: hidden;
+  background-color: white;
+  color: black;
+  border-radius: 50%;
+  padding: 4%;
+  flex-wrap: wrap;
+  margin: 5%;
+`;
+
+const Main = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Radius = styled.div`
-    background-color: #EFEFEF;
-    border-radius: 10% 10% 0 0;
-    flex: 1;
-`
+  background-color: #efefef;
+  border-radius: 95% 95% 0 0;
+  margin-top: -30px;
+  color: #efefef;
 
-const Main = styled.div`
-    min-height: 100vh;
+  p {
     display: flex;
-    flex-direction: column;
-`
+    justify-content: center;
+  }
+`;
 
 const Loteria = ({ data, id, loteria, numeros, loterias, children }) => {
   const numerosSorteados = numeros.map((numero) => {
-    return <li key={numero}>{numero}</li>;
+    return <Numeros key={numero}>{numero}</Numeros>;
   });
 
   const loteriaAtual = loterias.find((l) => {
@@ -48,8 +93,13 @@ const Loteria = ({ data, id, loteria, numeros, loterias, children }) => {
         <h3>Concurso Nº{id}</h3>
       </Background>
       <Radius>
-        <ul>{numerosSorteados}</ul>
+        <p>....................................</p>
+        <br></br>
+        <br></br>
       </Radius>
+      <NumberDiv>
+        <ul>{numerosSorteados}</ul>
+      </NumberDiv>
     </Main>
   );
 };
